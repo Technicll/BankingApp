4801e3d708670b9f24c596308672619b0188f2ab
duc-winn <131634380+duc-winn@users.noreply.github.com>
2024-11-16 17:47:58 -0600

    set up server and connect to database

--- BankingAppBackend/BankingAppBackend/src/main/java/com/example/demo/BankingAppBackendApplication.java
+++ BankingAppBackend/BankingAppBackend/src/main/java/com/example/demo/BankingAppBackendApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankingAppBackendApplication {
 
 	public static void main(String[] args) {
+		//this is a comment
 		SpringApplication.run(BankingAppBackendApplication.class, args);
-	}
+	} 
 
 }
